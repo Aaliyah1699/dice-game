680d6551fe631f9dd89d41ed717138f957aae49c
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-03 18:36:37 +0000

    fixed bug line 18

--- dice.py
+++ dice.py
@@ -1,63 +1,50 @@
 '''Roll Dice Game'''
-#import random module
+# import random module
 import random
-#import os module to clear screen after dice are rolled
+# import os module to clear screen after dice are rolled
 import os
 
-#Function to pick number if dice
+# Function to pick number if dice
+
+
 def dice_num():
-    #while True loop 
-    while True:
-        #Try - get user input for number of dice & a var list with valid responses
-        try:
+    while True:# while True loop
+        try:#Try get user input for number of dice
             pick_dice = input('Number of Dice: ')
-            valid = ['1','one','2', 'two']
-            #If input not in valid response raise value error with explanation
-            if pick_dice not in valid:
-                raise ValueError('Pick 1 or 2 dice.')
-            #Else return function
-            else:
-                return dice_num()
-        #Except with value error as err and print err
-        except ValueError as err:
+            valid = ['one', 'two']#var list with valid responses
+            if pick_dice not in valid:#If response not valid
+                raise ValueError('Pick one or two dice.')#raise value error with explanation
+            else:# Else return function
+                return pick_dice
+        except ValueError as err:# Except with value error as err and print err
             print(err)
 
-#Function to roll the dice
+# Function to roll the dice
+
+
 def roll_dice():
-    #min value of dice
-    min_val = 1
-    #max value of dice
-    max_val = 6
-    #roll dice again option
-    roll_again = 'y'
-
-    #While roll again is yes call amount dice num
-    while roll_again.lower() == 'yes' or roll_again.lower() == 'y':
-        # use os to clear screen 
-        os.system('cls' if os.name == 'nt' else 'clear')
-        amount = dice_num()
-
-        #if user choses 2 dice print values and sum of dice
-        if amount == '2' or amount == 'two':
+    min_val = 1# min value of dice
+    max_val = 6# max value of dice
+    roll_again = 'yes'# roll dice again option
+    while roll_again.lower() == 'yes':# While roll again is yes
+        os.system('cls' if os.name == 'nt' else 'clear')# use os to clear screen
+        amount = dice_num()#call amount dice num func
+        # if user choses 2 dice print values and sum of dice
+        if amount == 'two':
             print('Lets roll the dice...')
             dice1 = random.randint(min_val, max_val)
             dice2 = random.randint(min_val, max_val)
-            #print values and sum of dice
-            print('Dice One: ',dice1)
-            print('Dice Two: ',dice2)
-            print('Total= ', dice1 + dice2)
-            #Ask if they want to roll again
-            roll_again = input('Want to roll again? ')
-        #else if the pick one dice
-        else:
+            # print values and sum of dice
+            print('Dice One: ', dice1)
+            print('Dice Two: ', dice2)
+            print('Total: ', dice1 + dice2)
+            roll_again = input('Want to roll again? ') # Ask if they want to roll again
+        else:# else if the pick one dice
             print('Lets roll the die...')
             dice1 = random.randint(min_val, max_val)
-            #print value with f string
-            print(f'The value is: {dice1}')
-            #Ask if they want to roll again
-            roll_again = input('Want to toll again? ')
+            print(f'The value is: {dice1}')#print value with f string
+            roll_again = input('Want to roll again? ')
 
 # execute func if its called
 if __name__ == '__main__':
     roll_dice()
-
