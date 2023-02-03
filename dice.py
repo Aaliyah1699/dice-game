18837da4332c7d9463436e029f54bff6b7ee9666
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-03 18:49:28 +0000

    fixed comment on bug

--- dice.py
+++ dice.py
@@ -8,27 +8,29 @@ import os
 
 
 def dice_num():
-    while True:# while True loop
-        try:#Try get user input for number of dice
+    while True:  # while True loop
+        try:  # Try get user input for number of dice
             pick_dice = input('Number of Dice: ')
-            valid = ['one', 'two']#var list with valid responses
-            if pick_dice not in valid:#If response not valid
-                raise ValueError('Pick one or two dice.')#raise value error with explanation
-            else:# Else return function
+            valid = ['one', 'two']  # var list with valid responses
+            if pick_dice not in valid:  # If response not valid
+                # raise value error with explanation
+                raise ValueError('Pick one or two dice.')
+            else:  # Else return input
                 return pick_dice
-        except ValueError as err:# Except with value error as err and print err
+        except ValueError as err:  # Except with value error as err and print err
             print(err)
 
 # Function to roll the dice
 
 
 def roll_dice():
-    min_val = 1# min value of dice
-    max_val = 6# max value of dice
-    roll_again = 'yes'# roll dice again option
-    while roll_again.lower() == 'yes':# While roll again is yes
-        os.system('cls' if os.name == 'nt' else 'clear')# use os to clear screen
-        amount = dice_num()#call amount dice num func
+    min_val = 1  # min value of dice
+    max_val = 6  # max value of dice
+    roll_again = 'yes'  # roll dice again option
+    while roll_again.lower() == 'yes':  # While roll again is yes
+        # use os to clear screen
+        os.system('cls' if os.name == 'nt' else 'clear')
+        amount = dice_num()  # call amount dice num func
         # if user choses 2 dice print values and sum of dice
         if amount == 'two':
             print('Lets roll the dice...')
@@ -38,12 +40,14 @@ def roll_dice():
             print('Dice One: ', dice1)
             print('Dice Two: ', dice2)
             print('Total: ', dice1 + dice2)
-            roll_again = input('Want to roll again? ') # Ask if they want to roll again
-        else:# else if the pick one dice
+            # Ask if they want to roll again
+            roll_again = input('Want to roll again? ')
+        else:  # else if the pick one dice
             print('Lets roll the die...')
             dice1 = random.randint(min_val, max_val)
-            print(f'The value is: {dice1}')#print value with f string
+            print(f'The value is: {dice1}')  # print value with f string
             roll_again = input('Want to roll again? ')
+
 
 # execute func if its called
 if __name__ == '__main__':
