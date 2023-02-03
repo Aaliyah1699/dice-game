c01201779cc78dc7020227f76a656d8c021becbb
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-03 03:03:45 +0000

    num of dice func complete

--- dice.py
+++ dice.py
@@ -1,5 +1,23 @@
 '''Roll Dice Game'''
-# import random module
+#import random module
 import random
-# import os module
+#import os module to clear screen after dice are rolled
 import os
+
+#Function to pick number if dice
+def dice_num():
+    #while True loop 
+    while True:
+        #Try - get user input for number of dice & a var list with valid responses
+        try:
+            pick_dice = input('Number of Dice: ')
+            valid = ['1','one','2', 'two']
+            #If input not in valid response raise value error with explanation
+            if pick_dice not in valid:
+                raise ValueError('Pick 1 or 2 dice.')
+            #Else return function
+            else:
+                return dice_num()
+        #Except with value error as err and print err
+        except ValueError as err:
+            print(err)
